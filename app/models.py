43d886145b6d71d349b4836e84d5848d450cdf69
mxamin <amin.solhizadeh@gmail.com>
2015-04-30 16:20:36 +0430

    Add Database `Entry` Model

--- app/models.py
+++ app/models.py
@@ -1,1 +1,10 @@
 from app import db
+
+
+class Entry(db.Model):
+    __tablename__ = 'entries'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    title = db.Column(db.String(64), nullable=False)
+    text = db.Column(db.Text, nullable=False)
--- manage.py
+++ manage.py
@@ -6,13 +6,14 @@ import logging
 from flask.ext.script import Manager, Shell
 
 from app import create_app, db
+from app.models import Entry
 
 app = create_app(os.getenv('EXFLASKR_CONFIG') or 'default')
 manager = Manager(app)
 
 
 def make_shell_context():
-    return dict(app=app, db=db)
+    return dict(app=app, db=db, Entry=Entry)
 manager.add_command('shell', Shell(make_context=make_shell_context))
 
 
